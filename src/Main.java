2a8ec706742be3c00303178a47c8b8b84e2646d4
nurgazy-07 <temiralievnurgazy07@gmail.com>
2023-10-30 16:29:26 +0600

    init

---
+++ src/Main.java
@@ -0,0 +1,86 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+//        int number = new Scanner(System.in).nextInt();
+//        System.out.println(number);
+//        if (number < 2)
+//            hello();
+//        else {
+//            bye();
+//        }
+//
+//
+//    }
+//    public static void hello(){
+//        System.out.println("Hello");
+//
+//    }
+//    public  static void bye(){
+//        System.out.println("Bye");
+
+
+        int[] numbers = {23, 10, 1, 4, 21, 10, 30, 40, 5, 89, 90, 38, 77, 2, 17, 3};
+//       1. getMaxNumber(numbers);
+//       2. int[] array = sortArray(numbers);
+//        for (int i = 0; i < args.length; i++) {
+//            System.out.println(array[i]+ " ");
+//        }
+//       3, int
+
+
+    }
+//    1. масивдин ичиндеги эн чон элементти тап, метод колдонуп
+//        public static int getMaxNumber(int[] array){
+//            int maxNumber = array[0];
+//            for (int i = 0; i < array.length; i++) {
+//                if (array[i] > maxNumber){
+//                    maxNumber = array[i];
+//                }
+//            }
+//            return maxNumber;
+//        }
+//     2. массивдин осуу тартибинде сортто. кайтаруучу метод колдонуп
+//    public static int[] sortArray(int[] array){
+//        for (int i = 0; i < array.length - 1; i++) {
+//            for (int j = 0; j < array.length - 1; j++) {
+//                if (array[j] > array[j + 1]) {
+//                    int copyElement = array[j];
+//                    array[j] = array[j + 1];
+//                    array[j + 1] = copyElement;
+//                }
+//            }
+//        }
+//        return array;
+//    }
+//       учунчу тапшыпма
+//      public static int[] hetRandomArray(int arrayLength, startNumber, int endNumber ){
+//        int[] array = new int[arrayLength];
+//          for (int i = 0; i < array.length; i++) {
+//              array [i] = new Random().nextInt(startNumber, endNummer);
+//          }
+//          return array;
+//      }
+//    
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
